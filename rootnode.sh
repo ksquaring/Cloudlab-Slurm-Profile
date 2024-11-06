5401047c4deba3dbdb1964f00fda28ea253ee3c9
Kedar Prasad <kdr.prsd@gmail.com>
2024-11-05 22:31:31 -0500

    fix syntax

--- rootnode.sh
+++ rootnode.sh
@@ -16,7 +16,7 @@ systemctl restart munge
 sudo apt install slurm-wlm
 cp /local/repository/slurm.conf /etc/slurm/slurm.conf
 echo "NodeName=node[0] CPUs=32 CoresPerSocket=8 ThreadsPerCore=2 State=UNKNOWN
-NodeName=node[1:${1}] CPUs=32 CoresPerSocket=8 ThreadsPerCore=2 State=UNKNOWN
+NodeName=node[1-${1}] CPUs=32 CoresPerSocket=8 ThreadsPerCore=2 State=UNKNOWN
 PartitionName=debug Nodes=ALL Default=YES MaxTime=INFINITE State=UP" | sudo tee -a /etc/slurm/slurm.conf
 cp /local/repository/cgroup.conf /etc/slurm/cgroup.conf
 sleep 1
