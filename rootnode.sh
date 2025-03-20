4ace877d30c2d7e38d110fe93543ee7a904edbee
Kedar Prasad <kdr.prsd@gmail.com>
2025-03-19 22:49:07 -0400

    hi understudies

--- rootnode.sh
+++ rootnode.sh
@@ -18,6 +18,7 @@ cp /local/repository/slurm.conf /etc/slurm/slurm.conf
 echo "NodeName=node[0] CPUs=32 CoresPerSocket=8 ThreadsPerCore=2 State=UNKNOWN
 NodeName=node[1-${1}] CPUs=32 CoresPerSocket=8 ThreadsPerCore=2 State=UNKNOWN
 PartitionName=debug Nodes=ALL Default=YES MaxTime=INFINITE State=UP" | sudo tee -a /etc/slurm/slurm.conf
+
 cp /local/repository/cgroup.conf /etc/slurm/cgroup.conf
 sleep 1
 systemctl enable slurmctld
